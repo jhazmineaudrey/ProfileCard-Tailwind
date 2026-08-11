87d2f6ed54db61369a907ef0a3d0127c1445aa28
Jhaz <jhazmineaudrey7@gmail.com>
2026-08-12 01:20:36 +0800

    feat: add dark mode toggle

--- public/app.js
+++ public/app.js
@@ -1,5 +1,15 @@
 const card = document.querySelector(".cards");
+const darkBtn = document.querySelector("#DARK-MODE-TOGGLE");
+const body = document.querySelector("body");
 
 card.addEventListener("click", () => {
     card.classList.toggle("flipped");
 });
+
+darkBtn.addEventListener("click", () => {
+    if (!(body.dataset.theme === "dark")) {
+        body.dataset.theme = "dark"
+    } else {
+        body.dataset.theme = "light"
+    }
+})
